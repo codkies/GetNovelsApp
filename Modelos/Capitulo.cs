2d70fa7c368f02cb1b557640270f4c96cffbd562
Juan Ramos <60495132+codkies@users.noreply.github.com>
2020-09-18 15:40:20 -0400

    v0.5 Namespaces organized

---
+++ Modelos/Capitulo.cs
@@ -0,0 +1,16 @@
+﻿namespace GetNovelsApp.Modelos
+{
+    public struct Capitulo
+    {
+        public string Texto;
+        public int NumeroCapitulo;
+
+        public int Caracteres => Texto.Length;
+
+        public Capitulo(string texto, int numeroCapitulo)
+        {
+            Texto = texto;
+            NumeroCapitulo = numeroCapitulo;
+        }
+    }
+}
