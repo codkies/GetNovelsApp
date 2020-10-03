7ba74064ebea3e42dc7626fff4d235ff1416197a
Juan Ramos <60495132+codkies@users.noreply.github.com>
2020-10-03 01:28:17 -0400

    Clase Archivador separa en 3 archivos
    Metodos viejos
    Metodos nuevos para guardar novelas & caps
    Metodos de configuraciones y perfiles

---
+++ GetNovelsApp.Core/Conexiones/BaseDatos/ArchivadorConfiguracion.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using GetNovelsApp.Core.ConfiguracionApp;
+using GetNovelsApp.Core.ConfiguracionApp.xPaths;
+
+namespace GetNovelsApp.Core.Conexiones.DB
+{
+    /// <summary>
+    /// Relacionados a manipulacion de Perfiles websites
+    /// </summary>
+    public partial class Archivador
+    {
+        private const string TablaPerfilesWebsites = "PerfilesWebsites";
+        private const string TablaXPathTextos = "xPathsTextos";
+        private const string TablaXPathLinks = "xPathsLinks";
+        private const string TablaXPathTitulo = "xPathsTitulo";
+        private const string TablaConfiguracion = "Configuracion";
+        private const int IDdescendiente = 1;
+        private const int IDascendiente = 2;
+
+
+        public IConfig ObtenConfiguracion()
+        {
+            string qry = $"Select * from {TablaConfiguracion}";
+            using IDbConnection cnn = DataBaseAccess.GetConnection();
+            var dynamic = new { CarpetasDescarga = "", CapitulosPorDocumento = 0, TamañoBatch = 25 };
+            var resultado = cnn.Query(qry, dynamic);
+
+
+            string CarpetasDescarga = GetProperty(resultado, "CarpetasDescarga") as string;
+            int CapitulosPorDocumento = Convert.ToInt32(GetProperty(resultado, "CapitulosPorDocumento"));
+            int TamañoBatch = Convert.ToInt32(GetProperty(resultado, "TamañoBatch"));
+
+            return GetNovelsFactory.FabricaConfiguracion(CarpetasDescarga, TamañoBatch, CapitulosPorDocumento);
+        }
+
+
+        public void ActualizaConfiguracion(IConfig config)
+        {
+            string qry = $"update {TablaConfiguracion} " +
+                            $"set CarpetaDescargas = '{config.FolderPath}', " +
+                            $"TamañoBatch='{config.TamañoBatch}', " +
+                            $"CapitulosPorDocumento='{config.CapitulosPorDocumento}' ";
+            using IDbConnection cnn = DataBaseAccess.GetConnection();
+            cnn.Execute(qry);
+
+            GetNovelsEvents.Invoke_ConfiguracionCambio();
+        }
+
+
+        public bool PerfilExiste(string dominio)
+        {
+            using IDbConnection cnn = DataBaseAccess.GetConnection();
+            string qry = $"select * from {TablaPerfilesWebsites} where Dominio = '{dominio}'";
+            var output = cnn.Query(qry);
+            return output.Any();
+        }
+
+
+        public void GuardaPerfil(IPath Website)
+        {
+            //Tomando referencias.
+            string Dominio = Website.Dominio;
+            List<string> xPathsTextos = Website.xPathsTextos;
+            List<string> xPathsLinks = Website.xPathsLinks;
+            List<string> xPathsTitulo = Website.xPathsTitulo;
+            int OrdenID = (int)Website.OrdenLinks;
+
+            //Abriendo una conexión a la DB
+            using IDbConnection cnn = DataBaseAccess.GetConnection();
+
+            //Metiendo el Dominio
+            string qry_InsertDominio = InsertaDominio_Query(Dominio);
+            cnn.Execute(qry_InsertDominio);
+
+            //Obteniendo el ID del Dominio
+            string qry_ObtenWebsiteID = ObtenWebsiteID_Query(Dominio);
+            int WebsiteID = cnn.Query<int>(qry_ObtenWebsiteID).First();
+
+            //Metiendo los xPathsLinks
+            foreach (string xPath in xPathsLinks)
+            {
+                string qry_InsertarXPathsTextos = InsertaXPathLinks_Query(OrdenID, WebsiteID, xPath);
+                cnn.Execute(qry_InsertarXPathsTextos);
+            }
+
+            //Metiendo xPathsTextos
+            foreach (string xPath in xPathsTextos)
+            {
+                string qry_InsertarXPathsTextos = InsertaXPath_Query(WebsiteID, xPath, TablaXPathTextos);
+                cnn.Execute(qry_InsertarXPathsTextos);
+            }
+
+            //Metiendo xPathsTitulo
+            foreach (string xPath in xPathsTitulo)
+            {
+                string qry_InsertarXPathsTextos = InsertaXPath_Query(WebsiteID, xPath, TablaXPathTitulo);
+                cnn.Execute(qry_InsertarXPathsTextos);
+            }
+
+            GetNovelsEvents.Invoke_WebsitesCambiaron();
+        }
+
+
+        public List<IPath> SacaPerfiles()
+        {
+            using IDbConnection cnn1 = DataBaseAccess.GetConnection();
+
+            //Obteniendo dominio & ID
+            string dominioQry = $"select * from {TablaPerfilesWebsites}";
+            var modeloPerfiles = new { WebsiteID = 0, Dominio = "" };
+            var ListaPerfiles = cnn1.Query(dominioQry, modeloPerfiles).ToList();
+
+            List<Website> output = new List<Website>();
+
+            foreach (var perfil in ListaPerfiles)
+            {
+                using IDbConnection cnn = DataBaseAccess.GetConnection();
+                string Dominio = GetProperty(perfil, "Dominio");
+                int id = Convert.ToInt32(GetProperty(perfil, "WebsiteID"));
+
+                //Obteniendo xPath
+                string TituloQry = $"select xPath from {TablaXPathTitulo} where WebsiteID = '{id}'";
+
+                string LinkQry = $"select xPath from {TablaXPathLinks} where WebsiteID = '{id}'";
+                string OrdenQry = $"select OrdenID from {TablaXPathLinks} where WebsiteID = '{id}'";
+
+                string TextoQry = $"select xPath from {TablaXPathTextos} where WebsiteID = '{id}'";
+
+                List<string> xPathsTitulo = null;
+                List<string> xPathsLinks = null;
+                List<string> xPathsTexto = null;
+                OrdenLinks orden = 0;
+
+                xPathsTitulo = cnn.Query<string>(TituloQry).ToList();
+                xPathsLinks = cnn.Query<string>(LinkQry).ToList();
+                xPathsTexto = cnn.Query<string>(TextoQry).ToList();
+                orden = cnn.Query<OrdenLinks>(OrdenQry).First();
+
+
+                Website wb = new Website(Dominio, xPathsLinks, xPathsTexto, xPathsTitulo, orden);
+                output.Add(wb);
+            }
+
+            return new List<IPath>(output);
+        }
+
+
+        public static object GetProperty(object target, string name)
+        {
+            var site = System.Runtime.CompilerServices.CallSite<Func<System.Runtime.CompilerServices.CallSite, object, object>>.Create(Microsoft.CSharp.RuntimeBinder.Binder.GetMember(0, name, target.GetType(), new[] { Microsoft.CSharp.RuntimeBinder.CSharpArgumentInfo.Create(0, null) }));
+            return site.Target(site, target);
+        }
+
+
+
+
+
+        #region Queries
+
+        private static string ObtenWebsiteID_Query(string Dominio)
+        {
+
+            //agarrando su ID
+            return $"SELECT WebsiteID FROM {TablaPerfilesWebsites} " +
+                                            $"WHERE Dominio = '{Dominio}' ";
+        }
+
+        private static string InsertaDominio_Query(string Dominio)
+        {
+
+            //Guardando el Dominio
+            return $"INSERT INTO {TablaPerfilesWebsites} " +
+                                        $"(Dominio) values " +
+                                        $"('{Dominio}')";
+        }
+
+        private static string InsertaXPathLinks_Query(int OrdenID, int WebsiteID, string xPath)
+        {
+            return $"INSERT INTO {TablaXPathLinks} " +
+                                                $"(WebsiteID, xPath, OrdenID) values " +
+                                                $"('{WebsiteID}', \"{xPath}\", '{OrdenID}')";
+        }
+
+        private static string InsertaXPath_Query(int WebsiteID, string xPath, string Tabla)
+        {
+            return $"INSERT INTO {Tabla} " +
+                    $"(WebsiteID, xPath) values " +
+                    $"('{WebsiteID}', \"{xPath}\")";
+        }
+
+        #endregion
+
+
+    }
+
+}
---
+++ GetNovelsApp.Core/Conexiones/BaseDatos/ArchivadorViejo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using GetNovelsApp.Core.Conexiones.Internet;
+using GetNovelsApp.Core.Modelos;
+using GetNovelsApp.Core.Reportaje;
+
+namespace GetNovelsApp.Core.Conexiones.DB
+{
+    /// <summary>
+    /// Metodos "viejos" de console UI
+    /// </summary>
+    public partial class Archivador : IReportero
+    {
+        /// <summary>
+        /// Regresa una instancia de una novela registrada en la base de datos.
+        /// </summary>
+        /// <param name="infoNov"></param>
+        /// <returns></returns>
+        public INovela MeteNovelaDB(Uri LinkNovela, out bool YaExiste)
+        {
+            YaExiste = NovelaExisteEnDB(LinkNovela);
+            if (!YaExiste)
+            {
+                using IDbConnection cnn = DataBaseAccess.GetConnection();
+
+                //Obteniendo toda la info de la web y metiendola en la DB:
+                InformacionNovelaDB novDBInfo = InsertaNovelaEnDB(LinkNovela, cnn, out InformacionNovelaOnline infoNov);
+
+                //Capitulos:
+                List<Capitulo> CapitulosNovela = GetNovelsFactory.FabricaCapitulos(infoNov.LinksDeCapitulos);
+                GuardaCapitulos(CapitulosNovela, novDBInfo.ID); //Itera los caps y encuentra su info.
+
+                //Regresando una novela para runtime:
+                INovela nov = GetNovelsFactory.ObtenNovela(CapitulosNovela, novDBInfo);
+                cnn.Dispose();
+                return nov;
+            }
+            else
+            {
+                return SacaNovelaDB(LinkNovela);
+            }
+        }
+
+        /// <summary>
+        /// Crea una novela en la DB. Regresa un NovelDBmodel y un out NovelaWebModel
+        /// </summary>
+        private InformacionNovelaDB InsertaNovelaEnDB(Uri LinkNovela, IDbConnection cnn, out InformacionNovelaOnline infoNov)
+        {
+            //Encontrando informacion de la web.
+            infoNov = ManipuladorDeLinks.EncuentraInformacionNovela(LinkNovela);
+
+            //Insertando la novela
+            string qry = InsertNovel_Query(infoNov);
+            cnn.Execute(qry);
+
+            //Insertando las tagas
+            string qID = $"select ID from {TablaNovelas} where LinkPrincipal = \"{LinkNovela}\" ";
+            int novelID = cnn.Query<int>(qID).First();
+            string qry_tags = InsertTags_Query(infoNov, novelID);
+            cnn.Execute(qry_tags);
+
+
+            //Obteniendo una referencia de la DB
+            string query_ObtenDBInfo = $"SELECT n.id, n.Titulo, n.LinkPrincipal, n.Sipnosis, n.Imagen, t.Tags " +
+                                        $"from {TablaNovelas} as n " +
+                                        $"join {TablaTags} as t " +
+                                        $"on n.ID = t.NovelaID and n.id = {novelID}";
+
+            InformacionNovelaDB novDBInfo = cnn.Query<InformacionNovelaDB>(query_ObtenDBInfo).First();
+
+
+            return novDBInfo;
+        }
+
+    }
+}
